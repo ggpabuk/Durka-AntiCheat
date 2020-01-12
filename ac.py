dcf0501f179cb6bd92c83232a9f14d4457a40bd7
ggpabuk <59561857+ggpabuk@users.noreply.github.com>
2020-01-12 19:55:50 +0300

    Update ac.py

--- ac.py
+++ ac.py
@@ -6,10 +6,7 @@ import sys
 # Настройки
 proc = "GeometryDash.exe" # Имя процесса
 sleep = 5 # Задержка перед следующей проверкой
-debug = 0 # Режим отладки
-
-if debug != 1 or 0:
-	debug = 0
+debug = False # Режим отладки
 
 # Анти чит
 def main():
@@ -17,13 +14,13 @@ def main():
 	while True:
 		sign_new = subprocess.check_output('listdlls.exe ' + proc)
 		if sign == sign_new:
-			if debug == 1:
+			if debug == True:
 				print("Сигнатуры совпали")
 		else:
-			if debug == 1:
+			if debug == True:
 				print("Сигнатуры НЕ совпали")
 			os.system("taskkill /f /im " + proc)
-			# sys.exit() # Завершение процесса AC
+			sys.exit() # Завершение процесса AC
 		time.sleep(sleep)
 
 if __name__ == '__main__':
