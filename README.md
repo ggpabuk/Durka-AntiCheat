3968273a4baecce72303fc47f058c381c3ed8623
ggpabuk <generalpavuk@gmail.com>
2020-02-17 10:48:03 +0300

    Anti-Cheat rewrite

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 2. Launch the game 
 3. Done
 ----------------------------------------------------------------------------------
+*Need: [listdlls.exe]: https://docs.microsoft.com/en-us/sysinternals/downloads/listdlls*
 *Note: This is an OBT (Open Beta Testing) version of the program, break it to find bugs.*
-----------------------------------------------------------------------------------
-*Request: Do not change file names if this is not part of the testing.*
 ----------------------------------------------------------------------------------
--- ac.py
+++ ac.py
@@ -1,42 +1,44 @@
 import subprocess
 import time
+import json
 import os
 import sys
 
-arg = sys.argv
-debug = False
+argv = sys.argv
+debug = False				# Enable debug by default?
 
-# Настройки
-proc = "GeometryDash.exe" # Имя процесса
-sleep = 5 # Задержка перед следующей проверкой
-help_txt = '''Агрументы:
-Помощь: -h или --help
-Режим отладки: --debug
-'''
+with open('conf.json') as target:
+	conf = json.load(target)
 
-# Анти чит
-def main():
-	try:
-		if arg[1] == "--debug":
-			debug = True
-		elif arg[1] == "-h" or arg[1] == "--help":
-			print(help_txt)
-			sys.exit()
-	except IndexError:
-		print("Запуск без агрументов!")
+proc = conf['process']        # Process name
+sleep = conf['time_sleep']    # Delay before next check
+help_txt = conf['help_text']  # Help text
 
-	sign = subprocess.check_output('listdlls.exe ' + proc)
-	while True:
-		sign_new = subprocess.check_output('listdlls.exe ' + proc)
-		if sign == sign_new:
-			if debug == True:
-				print("Сигнатуры совпали")
-		else:
-			if debug == True:
-				print("Сигнатуры НЕ совпали")
-			os.system("taskkill /f /im " + proc)
-			sys.exit() # Завершение процесса AC
-			time.sleep(sleep)
+# Working with startup arguments
+try:
+	if argv[1] == "--debug":
+		debug = True
+	elif argv[1] == "-h" or argv[1] == "--help":
+		print(help_txt)
+		sys.exit()
+except IndexError:
+	pass
 
-if __name__ == '__main__':
-	main()
+
+# Main Anti-Cheat process
+sign = subprocess.check_output('listdlls.exe {proc}')
+while True:
+	sign_new = subprocess.check_output('listdlls.exe {proc}')
+	if sign_new != sign:
+		os.system("taskkill /f /im {proc}")
+		break
+	elif sign_new != sign and debug:
+		os.system("taskkill /f /im {proc}")
+		print("Signatures not match")
+		break
+	else:
+		print("Signatures match")
+
+	time.sleep(sleep)
+
+sys.exit()
